e76cf325e6ff47f70c137cba753c535a93d0ab0c
Yu Nahyun <155030721+iixanx@users.noreply.github.com>
2024-02-06 10:55:18 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 50380 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+nArr = set(map(int, input().split()))
+m = int(input())
+mArr = list(map(int, input().split()))
+
+for i in mArr:
+    if(i in nArr):
+        print(1)
+    else:
+        print(0)
