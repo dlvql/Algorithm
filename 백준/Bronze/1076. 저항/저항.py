8b19ee264d59fa80f8433b0414f6848c9a4db621
Yu Nahyun <155030721+iixanx@users.noreply.github.com>
2024-02-13 10:36:38 +0900

    [Bronze II] Title: 저항, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,6 @@
+arr = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+rst = arr.index(input())
+ond = arr.index(input())
+ird = 10 ** arr.index(input())
+
+print((rst * 10 + ond) * ird)
