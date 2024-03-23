2cea1fc0c6e6e78363f4e8f1aee6ea47689e0e3d
Yu Nahyun <155030721+iixanx@users.noreply.github.com>
2024-03-23 23:56:40 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.py
@@ -0,0 +1,15 @@
+ls = ['Never gonna give you up',
+'Never gonna let you down',
+'Never gonna run around and desert you',
+'Never gonna make you cry',
+'Never gonna say goodbye',
+'Never gonna tell a lie and hurt you',
+'Never gonna stop']
+
+n = int(input())
+isInLst = 'No'
+for i in range(n):
+    if(input() not in ls):
+        isInLst = 'Yes'
+        break
+print(isInLst)
