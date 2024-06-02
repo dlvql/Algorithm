61577abbb53849636418452ec54174e1ef1a2cf9
Yu Nahyun <155030721+iixanx@users.noreply.github.com>
2024-06-02 20:35:25 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,9 @@
+def solution(binomial):
+    a, op, b = binomial.split()
+    a, b = map(int, [a, b])
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    else:
+        return a * b
