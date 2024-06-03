ae6c37d24a6b4cfbb6b0faea16d9eedfaad8df94
Yu Nahyun <155030721+iixanx@users.noreply.github.com>
2024-06-03 13:56:38 +0900

    [level 0] Title: 세 개의 구분자, Time: 11.71 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,10 +1,3 @@
 def solution(myStr):
-    if set(myStr) == set(['a', 'b', 'c']):
-        return ["EMPTY"]
-    myStr = myStr.split('a')
-    ans, ret = [], []
-    for i in myStr:
-        ans += i.split('b')
-    for i in ans:
-        ret += i.split('c')
-    return list(filter(lambda x: x, ret))
+    myStr = list(filter(lambda x: x, myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split()))
+    return myStr if myStr else ["EMPTY"]
