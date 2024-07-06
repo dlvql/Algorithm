f9559cf692aa208ecc121d605d60b9da7a90821f
Yu Nahyun <155030721+iixanx@users.noreply.github.com>
2024-07-06 18:15:45 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+semina = {
+  'Algorithm' :	'204',
+  'DataAnalysis' :	'207',
+  'ArtificialIntelligence' :	'302',
+  'CyberSecurity' :	'B101',
+  'Network' :	'303',
+  'Startup' :	'501',
+  'TestStrategy' : '105'
+}
+
+for i in range(n):
+  print(semina[input()])
