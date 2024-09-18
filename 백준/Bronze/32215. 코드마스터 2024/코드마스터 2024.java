53715b7a2c047ca3697e432cdd27d2dfbdac182b
Yu Nahyun <iixiixanx@gmail.com>
2024-09-18 23:14:24 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 164 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt(), k = sc.nextInt();
+        System.out.println(k * m + m);
+    }
+}
