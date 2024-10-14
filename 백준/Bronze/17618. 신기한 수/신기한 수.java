96f490b2ea1c787beee33ca612254fabfce1a566
Yu Nahyun <iixiixanx@gmail.com>
2024-10-14 12:00:27 +0900

    [Bronze III] Title: 신기한 수, Time: 404 ms, Memory: 14892 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.java
+++ 백준/Bronze/17618. 신기한 수/신기한 수.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -8,12 +7,13 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int cnt = 0;
         for (int a = 1; a <= n; a++) {
-            int[] arr = Arrays.stream(String.valueOf(a).split("")).mapToInt(Integer::parseInt).toArray();
-            int s = 0;
-            for(int i: arr) {
+            int k = a, s = 0;
+            while (k > 0) {
+                int i = k % 10;
                 s += i;
+                k = (k - i) / 10;
             }
-            if (a % s == 0) cnt += 1;
+            if (a % s == 0L) cnt += 1;
         }
         bw.write(cnt + "");
         bw.flush();
