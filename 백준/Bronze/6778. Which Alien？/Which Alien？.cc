913825bafbe9bedc5ffef2b606c3546fc4913290
Yu Nahyun <iixiixanx@gmail.com>
2025-05-15 12:09:39 +0900

    [Bronze IV] Title: Which Alien?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.cc
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+  int antenna, eyes;
+  scanf("%d %d", &antenna, &eyes);
+  if (antenna >= 3 && eyes <= 4) printf("TroyMartian\n");
+  if (antenna <= 6 && eyes >= 2) printf("VladSaturnian\n");
+  if (antenna <= 2 && eyes <= 3) printf("GraemeMercurian\n");
+}
