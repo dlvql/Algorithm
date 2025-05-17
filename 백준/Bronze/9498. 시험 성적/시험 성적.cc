6f5249575ff00dea0a44211389a86eec6c68b8c0
Yu Nahyun <iixiixanx@gmail.com>
2025-05-17 09:44:08 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  int score;
+  scanf("%d", &score);
+  if (score >= 90) printf("A");
+  else if (score >= 80) printf("B");
+  else if (score >= 70) printf("C");
+  else if (score >= 60) printf("D");
+  else if (score < 60 || score > 100) printf("F");
+}
