92383213a246acdecff7b0c5eaf142ed431fccbb
Yu Doa <iixiixanx@gmail.com>
2025-05-31 10:49:32 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  if (n == 1) printf("11\nA B C D E F G H J L M");
+  if (n == 2) printf("9\nA C E F G H I L M");
+  if (n == 3) printf("9\nA C E F G H I L M");
+  if (n == 4) printf("9\nA B C E F G H L M");
+  if (n == 5) printf("8\nA C E F G H L M");
+  if (n == 6) printf("8\nA C E F G H L M");
+  if (n == 7) printf("8\nA C E F G H L M");
+  if (n == 8) printf("8\nA C E F G H L M");
+  if (n == 9) printf("8\nA C E F G H L M");
+  if (n == 10) printf("8\nA B C F G H L M");
+}
